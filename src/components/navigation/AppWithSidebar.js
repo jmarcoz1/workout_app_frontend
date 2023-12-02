4b722634d53d2d92fa94519cba14b35ab12158d6
Jorge Marco <jmararr1@gmail.com>
2023-12-02 17:20:51 +0100

    added some conditional rendering, more work lies ahead

--- src/components/navigation/AppWithSidebar.js
+++ src/components/navigation/AppWithSidebar.js
@@ -1,10 +1,12 @@
 import React, { useState } from 'react';
-import ListItemButton from '@mui/material/ListItemButton';
-import { AppBar, Toolbar, IconButton, Typography, Drawer, List, ListItem, ListItemText } from '@mui/material';
+import HomeIcon from '@mui/icons-material/Home';
+import FitnessCenterIcon from '@mui/icons-material/FitnessCenter';
+import { AppBar, Toolbar, IconButton, Typography, Drawer, List, ListItem, ListItemText, ListItemIcon, ListItemButton } from '@mui/material';
 import MenuIcon from '@mui/icons-material/Menu';
 
 const AppWithSidebar = () => {
   const [isDrawerOpen, setDrawerOpen] = useState(false);
+  const [selectedPage, setSelectedPage] = useState('profile'); // Default selected page
 
   const handleDrawerOpen = () => {
     setDrawerOpen(true);
@@ -12,6 +14,11 @@ const AppWithSidebar = () => {
 
   const handleDrawerClose = () => {
     setDrawerOpen(false);
+  };
+
+  const handlePageChange = (page) => {
+    setSelectedPage(page);
+    handleDrawerClose(); // Close the drawer after selecting a page
   };
 
   return (
@@ -35,10 +42,17 @@ const AppWithSidebar = () => {
         onClose={handleDrawerClose}
       >
         <List>
-          <ListItemButton onClick={handleDrawerClose}>
+          <ListItemButton onClick={() => handlePageChange('profile')} selected={selectedPage === 'profile'}>
+            <ListItemIcon>
+              <HomeIcon />
+            </ListItemIcon>
             <ListItemText primary="My profile" />
           </ListItemButton>
-          <ListItemButton onClick={handleDrawerClose}>
+
+          <ListItemButton onClick={() => handlePageChange('training')} selected={selectedPage === 'training'}>
+            <ListItemIcon>
+              <FitnessCenterIcon />
+            </ListItemIcon>
             <ListItemText primary="My training" />
           </ListItemButton>
         </List>
@@ -46,7 +60,21 @@ const AppWithSidebar = () => {
 
       {/* Main Content */}
       <div>
-        {/* Your main content goes here */}
+        {selectedPage === 'profile' && (
+          // Render the content for the 'My profile' page
+          <div>
+            <Typography variant="h4">My Profile Page</Typography>
+            {/* Add your profile page content here */}
+          </div>
+        )}
+
+        {selectedPage === 'training' && (
+          // Render the content for the 'My training' page
+          <div>
+            <Typography variant="h4">My Training Page</Typography>
+            {/* Add your training page content here */}
+          </div>
+        )}
       </div>
     </div>
   );
