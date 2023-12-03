22e36d100f561e6c5ff11a6657ce0fbc943f4767
Jorge Marco <jmararr1@gmail.com>
2023-12-03 10:30:46 +0100

    Login working

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import { Typography, Grid, Box } from '@mui/material';
 import ExerciseCard from './components/workout_components/ExerciseCard';
 import AppWithSidebar from './components/navigation/AppWithSidebar';
 import './App.css';
+import LoginPage from './components/pages/LoginPage';
 
 const App = () => {
   return (
-    <div>
-      <AppWithSidebar />
-    </div>
+    // <div>
+    //   <AppWithSidebar />
+    // </div>
+    <div><LoginPage/></div>
     
   );
 };
---
+++ src/components/pages/LoginPage.js
@@ -0,0 +1,76 @@
+import React, { useState, useHistory } from 'react';
+import axios from 'axios';
+import { Button, TextField, Container, Typography } from '@mui/material';
+
+const LoginPage = () => {
+
+    const [username, setUsername] = useState('');
+    const [password, setPassword] = useState('');
+    const history = useHistory();
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+
+        try {
+            const response = await axios.post('http://localhost:8000/api/token/', {
+                username: username,
+                password: password
+            });
+
+            localStorage.setItem('access', response.data.access);
+            localStorage.setItem('refresh', response.data.refresh);
+
+            // Redirect the user to the home page or dashboard
+            history.push('/home');  // replace '/home' with your actual home page route
+        } catch (error) {
+            console.error('An error occurred while logging in:', error);
+            // Handle the error here, for example by showing an error message to the user
+        }
+    }
+
+    return (
+        <Container component="main" maxWidth="xs">
+            <Typography component="h1" variant="h5">
+                Sign in
+            </Typography>
+            <form onSubmit={handleSubmit}>
+                <TextField
+                    variant="outlined"
+                    margin="normal"
+                    required
+                    fullWidth
+                    id="username"
+                    label="Username"
+                    name="username"
+                    autoComplete="username"
+                    autoFocus
+                    value={username}
+                    onChange={e => setUsername(e.target.value)}
+                />
+                <TextField
+                    variant="outlined"
+                    margin="normal"
+                    required
+                    fullWidth
+                    name="password"
+                    label="Password"
+                    type="password"
+                    id="password"
+                    autoComplete="current-password"
+                    value={password}
+                    onChange={e => setPassword(e.target.value)}
+                />
+                <Button
+                    type="submit"
+                    fullWidth
+                    variant="contained"
+                    color="primary"
+                >
+                    Sign In
+                </Button>
+            </form>
+        </Container>
+    );
+}
+
+export default LoginPage;
